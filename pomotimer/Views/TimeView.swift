6a2db3e08875ae534748bfdf7506229ab72b2db1
JWpark234 <pjw.developer.acc@gmail.com>
2024-01-19 22:02:26 +0700

    updated UI

---
+++ pomotimer/Views/TimeView.swift
@@ -0,0 +1,18 @@
+//
+//  TimeView.swift
+//  pomotimer
+//
+//  Created by Jinwoo Park on 2024/01/19.
+//
+
+import SwiftUI
+
+struct TimeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeView()
+}
